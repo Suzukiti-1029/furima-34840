e50bb3d36c0dfddcabdd1b90040cf2163340c3fd
Suzukiti-1029 <h40396suzujun@gmail.com>
2021-03-29 15:17:58 +0900

    名前入力欄とフリガナ入力欄のバリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,11 +9,11 @@ class User < ApplicationRecord
   PASSWORD_RULE = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/.freeze
   validates_format_of :password, with: PASSWORD_RULE, message: 'には英字と数字の両方を含めて設定してください'
 
-  with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+  with_options presence:true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
     validates :last_name
     validates :first_name
   end
-  with_options format: { with: /\A[ァ-ヶー]+\z/, message: '全角カタカナを使用してください' } do
+  with_options presence:true, format: { with: /\A[ァ-ヶー]+\z/, message: '全角カタカナを使用してください' } do
     validates :last_name_detail
     validates :first_name_detail
   end
