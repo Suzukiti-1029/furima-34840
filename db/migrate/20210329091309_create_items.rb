9181a02df236a8ae1ae6431e6b5993fb4a42729e
Suzukiti-1029 <h40396suzujun@gmail.com>
2021-03-29 18:19:54 +0900

    itemsテーブルの作成

--- db/migrate/20210329091309_create_items.rb
+++ db/migrate/20210329091309_create_items.rb
@@ -1,6 +1,15 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
+      t.string :name, null: false
+      t.text :describe, null: false
+      t.integer :category_id, null: false
+      t.integer :situation_id, null: false
+      t.integer :fare_option_id, null: false
+      t.integer :prefecture_id, null: false
+      t.integer :need_days_id, null: false
+      t.integer :feel, null: false
+      t.references :user, foreign_key: :true
       t.timestamps
     end
   end
