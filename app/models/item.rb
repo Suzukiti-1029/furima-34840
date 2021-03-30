30ff11073d205ed00138a0544aa4458430c19ad6
Suzukiti-1029 <h40396suzujun@gmail.com>
2021-03-30 10:53:46 +0900

    ActiveStorageのアソシエーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to [:category, :situation, :fare_option, :prefecture, :need_days]
+  has_one_attached :image
 end
