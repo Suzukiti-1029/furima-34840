fbc93c14702d7d37cac83eca4dc1e724420d7dd9
Suzukiti-1029 <h40396suzujun@gmail.com>
2021-03-30 12:40:19 +0900

    新規投稿のストロングパラメータの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,11 +5,21 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
   def create
-    @item = Item.new()
+    @item = Item.new(item_params)
+    binding.pry
     if @item.save
       redirect_to root_path
     else
       render :new
     end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(
+      :image, :name, :describe, :category_id, :situation_id, :fare_option_id, :prefecture_id, :need_days_id, :fee
+    ).merge(
+      user_id: current_user.id
+    )
   end
 end
