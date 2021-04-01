075d05dfc30d3bdd2c27f519a825310f47b54243
Suzukiti-1029 <h40396suzujun@gmail.com>
2021-04-01 11:35:25 +0900

    コントローラーの記述順序修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :move_to_root, only: [:edit, :update]
   before_action :set_item, only: [:show, :edit, :update]
+  before_action :move_to_root, only: [:edit, :update]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -44,11 +44,11 @@ class ItemsController < ApplicationController
     )
   end
 
-  def set_item
-    @item = Item.find(params[:id])
-  end
-
   def move_to_root
     redirect_to root_path if @item.user.id != current_user.id
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
