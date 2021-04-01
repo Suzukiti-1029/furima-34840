563b925278e786a9382fe24d8b5d8591f7acda5c
Suzukiti-1029 <h40396suzujun@gmail.com>
2021-04-01 19:53:46 +0900

    residenceモデルにバリデーションの設定

--- app/models/residence.rb
+++ app/models/residence.rb
@@ -1,4 +1,18 @@
 class Residence < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :prefecture
+  with_options presence: true do
+    validates :area_number, format: {
+        with: /\A\d{3}[-]\d{4}\z/,
+        message: '3ケタの半角数字、半角ハイフン(-)、4ケタの半角数字を続けて入力してください'
+        }
+    validates :city
+    validates :address
+    validates :phone_number, format: {
+        with: /\A\d{10,11}\z/,
+        message: '10ケタか11ケタの半角数字を続けて入力してください'
+      }
+  end
+
+  validates :prefecture_id, numericality: { other_than: 1 }
 end
