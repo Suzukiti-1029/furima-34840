47b955642fea688e64721ac7c40271aa2964b74a
Suzukiti-1029 <h40396suzujun@gmail.com>
2021-04-02 12:36:43 +0900

    formオブジェクトへバリデーションの移行・設置

--- app/models/residence.rb
+++ app/models/residence.rb
@@ -3,18 +3,4 @@ class Residence < ApplicationRecord
   has_one :history
 
   belongs_to :prefecture
-  with_options presence: true do
-    validates :area_number, format: {
-        with: /\A\d{3}[-]\d{4}\z/,
-        message: '3ケタの半角数字、半角ハイフン(-)、4ケタの半角数字を続けて入力してください'
-        }
-    validates :city
-    validates :address
-    validates :phone_number, format: {
-        with: /\A\d{10,11}\z/,
-        message: '10ケタか11ケタの半角数字を続けて入力してください'
-      }
-  end
-
-  validates :prefecture_id, numericality: { other_than: 1 }
 end
--- app/models/residence_purchase_history.rb
+++ app/models/residence_purchase_history.rb
@@ -3,4 +3,21 @@ class ResidencePurchaseHistory
   attr_accessor :area_number, :prefecture_id,
     :city, :address, :building, :phone_number,
     :user_id, :item_id
+
+  with_options presence: true do
+    validates :area_number, format: {
+        with: /\A\d{3}[-]\d{4}\z/,
+        message: '3ケタの半角数字、半角ハイフン(-)、4ケタの半角数字を続けて入力してください'
+      }
+    validates :city
+    validates :address
+    validates :phone_number, format: {
+        with: /\A\d{10,11}\z/,
+        message: '10ケタか11ケタの半角数字を続けて入力してください'
+      }
+    validates :user_id
+    validates :item_id
+  end
+
+  validates :prefecture_id, numericality: { other_than: 1 }
 end
