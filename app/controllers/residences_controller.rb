77cbb56aee55040a7b71d0b4f190e81a2f65db6b
Suzukiti-1029 <h40396suzujun@gmail.com>
2021-04-02 11:01:46 +0900

    非ログイン→ログインページ・出品者→トップページにそれぞれ遷移

--- app/controllers/residences_controller.rb
+++ app/controllers/residences_controller.rb
@@ -1,12 +1,13 @@
 class ResidencesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_item
+  before_action :move_to_root
   def new
     @residence = Residence.new
-    @item = Item.find(params[:item_id])
   end
 
   def create
     @residence = Residence.new(residence_params)
-    @item = Item.find(params[:item_id])
     if @residence.save
       redirect_to item_path(@item)
     else
@@ -19,6 +20,14 @@ class ResidencesController < ApplicationController
     params.require(:residence).permit(
       :area_number, :prefecture_id, :city, :address, :building, :phone_number
     )
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def move_to_root
+    redirect_to root_path if @item.user.id == current_user.id
   end
 end
 
