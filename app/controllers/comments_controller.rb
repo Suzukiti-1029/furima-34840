0ce3ad2b4bd534f8651d8e19cfaa5525e6c98910
Suzukiti-1029 <h40396suzujun@gmail.com>
2021-04-06 14:52:46 +0900

    comment#createの設定

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,19 @@
 class CommentsController < ApplicationController
+  def create
+    @item = Item.find(params[:item_id])
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      redirect_to root_path
+    else
+      render "items/show"
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:text).merge(
+      user_id: current_user.id,
+      item_id: @item.id
+    )
+  end
 end
