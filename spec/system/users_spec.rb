982346b24ac4c90d17827418d4ca70796ec1b564
Suzukiti-1029 <h40396suzujun@gmail.com>
2021-04-07 11:12:43 +0900

    ユーザーログイン機能の結合テスト記述

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -44,7 +44,7 @@ RSpec.describe 'ユーザー新規登録', type: :system do
   context 'ユーザー新規登録ができないとき' do
     it '誤った情報ではユーザー新規登録ができずに新規登録ページへ戻ってくる' do
       # トップページに移動する
-      basic_pass(root_path)
+      visit root_path
       # トップページにサインアップページへ遷移するボタンがあることを確認する
       expect(page).to have_content('新規登録')
       # 新規登録ページへ移動する
@@ -78,23 +78,39 @@ RSpec.describe 'ユーザーログイン', type: :system do
   context 'ログインができるとき' do
     it '保存されているユーザーの情報と一致すればログインができてトップページに移動する' do
       # トップページに移動する
+      visit root_path
       # トップページにログインページへ遷移するボタンがあることを確認する
+      expect(page).to have_content('ログイン')
       # ログインページへ移動する
+      visit new_user_session_path
       # ユーザー情報を入力する
+      fill_in 'user[email]', with: @user.email
+      fill_in 'user[password]', with: @user.password
       # ログインボタンを押す
+      click_on('ログイン')
       # トップページへ遷移したことを確認する
+      expect(current_path).to eq(root_path)
       # ログアウトボタンが表示されていることを確認する
+      expect(page).to have_content('ログアウト')
       # サインアップページへ遷移するボタンや、ログインページへ遷移するボタンが表示されていないことを確認する
+      expect(page).to have_no_content('新規登録')
+      expect(page).to have_no_content('ログイン')
     end
   end
   context 'ログインができないとき' do
     it '保存されているユーザーの情報と一致しないとログインができずにログインページへ戻ってくる' do
       # トップページに移動する
+      visit root_path
       # トップページにログインページへ遷移するボタンがあることを確認する
+      expect(page).to have_content('ログイン')
       # ログインページへ移動する
+      visit new_user_session_path
       # ログインボタンを押す
+      click_on('ログイン')
       # ログインページへ戻されることを確認する
+      expect(current_path).to eq(user_session_path)
       # エラーメッセージが画面に表示されていることを確認する
+      expect(page).to have_content('Invalid Email or password.')
     end
   end
 end
